cdbfdc405216bc9f75aac7091a737e1a91d5e770
Anchu26 <mahanaanchal526@gmail.com>
2026-07-09 10:59:17 +0530

    routing changes

--- sdplproject/src/pages/Home.jsx
+++ sdplproject/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import Header from "../components/Header";
+import Hero from "../components/Hero";
+import About from "../components/About";
 
 function App() {
   return (
     <>
       <Header />
+
+      <Hero />
+      <About />
+
     </>
   );
 }
